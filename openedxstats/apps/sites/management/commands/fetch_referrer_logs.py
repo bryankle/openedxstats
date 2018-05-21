0d1307ec422c3099bd621032e4e4b0b3935a45ab
Ned Batchelder <ned@edx.org>
2018-05-21 11:57:30 -0400

    Try to access the new bucket for stats

--- openedxstats/apps/sites/management/commands/fetch_referrer_logs.py
+++ openedxstats/apps/sites/management/commands/fetch_referrer_logs.py
@@ -125,7 +125,7 @@ def process_log_file(gz_file, log_name):
         log_to_save.save()
 
 
-def get_accessible_keys(bucket, prefix="edx-static-cloudfront"):
+def get_accessible_keys(bucket, prefix="edx-static"):
     accessible_keys = []
     for key in bucket.list(prefix=prefix):
         if key.storage_class != "GLACIER":
